0fe0d2fdd320834540b9bece9df8bec99c4cdf4b
pvermees <p.vermeesch@ucl.ac.uk>
2022-01-19 15:50:18 +0000

    Updated extra combinatorics exercise

--- quizzes/parsextra.R
+++ quizzes/parsextra.R
@@ -1,12 +1,17 @@
 idir <- "~/Documents/Programming/R/geostats/quizzes/"
 odir <- "~/Desktop/GEOL0061/"
-rmarkdown::render(paste0(idir,"extra.Rmd"))
+rmarkdown::render(input=paste0(idir,"extra.Rmd"),
+                  output_file=paste0(odir,"extra.pdf"))
 pages <- c(2,4,7,8,9,11,13,16,19,22)
 np <- length(pages)
 set.seed(1)
-NN <- round(runif(n=nq,min=0,max=20))
+if (TRUE){ # randomise
+    NN <- round(runif(n=np,min=0,max=20))
+} else {
+    NN <- rep("",np)
+}
 for (i in 1:(np-1)){
-    cmd <- paste0("pdftk ",idir,"extra.pdf cat ",
+    cmd <- paste0("pdftk ",odir,"extra.pdf cat ",
                   pages[i],"-",pages[i+1]-1,
                   " output ",odir,"q",i,NN[i],".pdf")
     system(cmd)
