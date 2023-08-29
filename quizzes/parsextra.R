583f47a7e325916c18e98e9ef5e2eacf657594ff
pvermees <p.vermeesch@ucl.ac.uk>
2023-08-29 17:38:50 +0100

    Added xlim, ylim and zlim options to colourplot() function

--- quizzes/parsextra.R
+++ quizzes/parsextra.R
@@ -3,7 +3,7 @@ odir <- "~/Desktop/GEOL0061/"
 rmarkdown::render(input=paste0(idir,"extra.Rmd"),
                   output_file=paste0(odir,"extra.pdf"))
 
-if (TRUE){ # parse
+if (FALSE){ # parse
     pages <- c(2,4,7,9,10,12,13,15,16,17,18,20,21,23,25,28,31,35,38,40,42)
     np <- length(pages)
     set.seed(1)
