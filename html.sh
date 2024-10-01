73aa4f978d13158d8ad49c0d971704be3d53aa0f
Pieter <p.vermeesch@ucl.ac.uk>
2024-10-01 23:11:24 +0100

    Fully reproducible figures and html code

--- html.sh
+++ html.sh
@@ -2,7 +2,11 @@ rm -rf html/*
 cp -r figures html
 cp -r latex html
 cd html/latex
-pdflatex geostats.tex
-pdflatex geostats.tex
-make4ht -d .. geostats.tex
-cd ../..
+rm *.log
+rm *.aux
+rm *.out
+rm *.toc
+pdflatex ./geostats.tex
+pdflatex ./geostats.tex
+pdflatex ./geostats.tex
+make4ht -d .. ./geostats.tex
