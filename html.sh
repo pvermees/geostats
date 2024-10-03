d65e31322971f1a9cef32c54572e28458a394219
Pieter <p.vermeesch@ucl.ac.uk>
2024-10-03 09:33:30 +0100

    Additional css editing in html

--- html.sh
+++ html.sh
@@ -15,3 +15,4 @@ pdflatex ./geostats.tex
 pdflatex ./geostats.tex
 make4ht ./geostats.tex
 sed -i 's/<\/head>/<style>\nbody\{width:800px;margin-left:auto;margin-right:auto;\}\nimg\{max-height:200px; max-width:800px\;\}\n<\/style>\n<\/head>/g' ./geostats.html
+sed -i 's/\.cmr\-6\\{/\.cmr\-6\\{padding\-right:1em;/g' ./geostats.css
